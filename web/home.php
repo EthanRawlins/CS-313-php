3d595510330ae529c900bfb9340e8f8fad15e747
ethan rawlins <raw16004@byui.edu>
2020-01-17 18:17:14 -0700

    Added Home page.

---
+++ web/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ethan Rawlins's Home Page</title>
+<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+
+<body>
+<h1>Ethan Rawlins</h1>
+<p>Ethan Rawlins is from <b>Virginia</b> and is studying with a <u>Software Engineering</u> major.</p>
+<div id="center_link">
+   <button class="button" onclick="window.location.href='/~ethanrawlins/index.html'">
+   <span>CS 213 Assignments</span>
+   </button>
+</div>
+<br>
+<div id="center_alert">
+<button class="button" type="button" onclick="alert('Ethan Rawlins has been married for a year, and plays the trumpet.')">More About Ethan</button>
+</div>
+<br>
+<hr>
+<br> <br>
+<div id="center_img">
+<img src="download.jpg" alt="Nature Pic" width="315" height="175">
+</div>
+<br>
+<blockquote cite="https://www.lds.org/scriptures/nt/philip/4?lang=eng">"I can do all things through Christ which strengtheneth me." (<i>Phillip. 4:13</i>)</blockquote>
+</body>
+</html>
