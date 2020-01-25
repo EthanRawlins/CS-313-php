ea66857cef411010c0e9e5a7b6e285dead38fb50
ethan rawlins <raw16004@byui.edu>
2020-01-25 16:56:14 -0700

    Added other files

--- web/assign03a.php
+++ web/assign03a.php
@@ -7,10 +7,10 @@
 </html>
 <?php
    if($_GET["order"] == "confirm") {
-   echo "<form action=\"assign03.html\">Yay! Your Order has been confirmed!<br><div style=\"margin: auto\"><img src=\"High-Five.jpeg\" alt=\"Virtual High Five\" style=\"margin: auto; height: 80%; width: 100%; max-width: 700px; max-height: 700px;\"></div><br><button type=\"submit\">Back to Checkout</button></form>";
+   echo "<form action=\"assign03-checkout.php\">Yay! Your Order has been confirmed!<br><div style=\"margin: auto\"><img src=\"High-Five.jpeg\" alt=\"Virtual High Five\" style=\"margin: auto; height: 80%; width: 100%; max-width: 700px; max-height: 700px;\"></div><br><button type=\"submit\">Back to Checkout</button></form>";
    }
    else if($_GET["order"] == "cancel") {
-   echo "<form action=\"assign03.html\">Noooooo!!!! Your Order has been cancelled! You'd better go back and buy something!<br><button type=\"submit\">Back to Checkout</button></form>";
+   echo "<form action=\"assign03-checkout.php\">Noooooo!!!! Your Order has been cancelled! You'd better go back and buy something!<br><button type=\"submit\">Back to Checkout</button></form>";
    }
 
 ?>
