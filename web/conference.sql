1f19cb18595b47dc2996108d561eaa4447d5e7be
ethan rawlins <raw16004@byui.edu>
2020-01-29 18:20:58 -0700

    Added conference sql file

---
+++ web/conference.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE conference;
+
+\c conference
+
+CREATE TABLE public.user
+(
+   id SERIAL NOT NULL PRIMARY KEY,
+   username VARCHAR(100) NOT NULL UNIQUE,
+   password VARCHAR(100) NOT NULL,
+   display_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE public.speaker
+(
+   id SERIAL NOT NULL PRIMARY KEY,
+   name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE public.note
+(
+   id SERIAL NOT NULL PRIMARY KEY,
+   user_id INT NOT NULL REFERENCES public.user(id),
+   speaker_id INT NOT NULL REFERENCES public.speaker(id),
+   conference_id INT NOT NULL REFERENCES public.conference(id),
+   note_text TEXT NOT NULL
+);
+
+
