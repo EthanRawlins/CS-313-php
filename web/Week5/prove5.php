f22be65b097411444b2259ad6d290f224a157b91
ethan rawlins <raw16004@byui.edu>
2020-02-07 20:18:50 -0700

    Display database

--- web/Week5/prove5.php
+++ web/Week5/prove5.php
@@ -28,8 +28,9 @@
 foreach ($db->query('SELECT * FROM item') as $row)
 {
    echo '$' . $row[2];
-   echo ' ' . $row[3];
-   echo ' $' . $row[4] . '<br><br>';
+   echo ' - ' . $row[3];
+   echo ' $' . $row[4] . '<br>';
+   echo $row[5] . '<br><br>';
 }
 ?>
 
